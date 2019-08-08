f9d8dbf4b17b11abd890b2e16a6dcf02242de128
Anubhab098 <52344641+Anubhab098@users.noreply.github.com>
2019-08-08 21:47:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+#######################
+####Main IMPORTS ######
+#######################
+
+import os
+import math
+import subprocess
+
+###################
+### MAIN MENU #####
+###################
+os.system("clear");
+os.system("figlet OS-X");
+print("1.OS-UBUNTU");
+print("2.OS-KALI");
+print("3.CREDITS");
+#######################
+### MAIN FUNCTION #####
+#######################
+
+selection=int(input("ENTER CHOICE :"))
+
+if selection ==1:
+  os.system("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent>
+
+if selection ==2:
+  os.system("pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent>
+
+if selection ==3:
+   print("CODED BY ANUBHAB , MAINTEND BY ANUBHAB , REPOSTIORY BY ANLINUX ,PATCH BY ANUBHAB, primarily maintend by anubhab");
