4ada453328a210c4b380353d40f335e7c273c13f
Anubhab098 <52344641+Anubhab098@users.noreply.github.com>
2019-08-08 22:09:12 +0530

    Update depends.sh

--- depends.sh
+++ depends.sh
@@ -14,7 +14,7 @@ chmod +x /data/data/com.termux/files/usr/bin
 echo "$blue [*] Updating Apt Cache"
 apt-get update -y && apt-get upgrade -y
 echo "$blue [*] Installing Dependencies"
-apt-get install figlet -y 
+apt-get install figlet -y && apt-get install python -y
 echo "$red [*] Fixing Permission Of OS-X SCRIPT"
 chmod +x main_x.py
 echo "$blue All Dependencies Installed And Srcipt is Usable Just Start it with python main_x.py"
